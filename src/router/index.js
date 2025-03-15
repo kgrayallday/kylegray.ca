4f60ccbb56e4c1e371bbbe8f23f11174e46b0a47
Kyle <kylemaxgray@gmail.com>
2025-03-15 16:48:01 -0700

    update blog route

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,7 @@ const router = createRouter({
     {
       path: '/blog',
       name: 'blog',
-      component: () => import('../views/MyBlog.vue'),
+      component: () => import('../views/BlogView.vue'),
     },
   ],
 })
