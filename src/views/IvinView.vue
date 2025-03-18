f132872c10127821bcd4ac2cf34b9b4b9f4944d8
Kyle <kylemaxgray@gmail.com>
2025-03-18 08:15:35 -0700

    add ivin assets

---
+++ src/views/IvinView.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>IVIN</h1>
+    <p>IVIN was a vehicle barcode scanner and data collection android application written for the logistic industry. </p>
+    <p>written using android studio in the Java programming language.</p>
+  </div>
+</template>
+
+<style>
+  
+</style>
