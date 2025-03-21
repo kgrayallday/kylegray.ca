67b73ed200c19a85aac0010e0119c53e0e542af7
Kyle <kylemaxgray@gmail.com>
2025-03-20 21:53:22 -0700

    style updates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,20 +2,25 @@
 </script>
 
 <template>
-  <div class='d-flex m-3' width="760px">
+  <div class='d-flex' width="760px">
     <div class='content'>
-      <div class='about d-flex'>
+      <div class='about d-flex mt-4'>
+        <div class="me-3">
         <p>Hello, I am a self-taught software developer. My home server is in <span class="text-red">Vancouver Canada</span>. This is my website that aims to be (amoung other things) my digital mind garden.
         </p>
+        </div>
+<!--        <div class="ms-3">
+          <img src="../../public/kylemain-square.jpg"/>
+</div>-->
       </div>
 
-      <div class="contact-card">
+      <div class="contact-card mt-4">
         <h2>Area 1</h2>
         <RouterLink to="/ivin">IVIN</RouterLink>
         <p>item 2</p>
       </div>
 
-      <div>
+      <div class="mt-4">
         <h2>Area 2</h2>
         <p>item 1</p>
         <p>item 2</p>
